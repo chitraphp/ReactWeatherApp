b557b9a05e1c553e2bf0055e78c3a1a8d2fa839a
Chitra Atmakuru <chitraphp@users.noreply.github.com>
2020-02-13 09:37:11 -0800

    add titles to app

--- src/App.js
+++ src/App.js
@@ -16,9 +16,10 @@ class App extends React.Component {
     error: undefined
   };
 
-  getWeather = async (e) => {
+  getWeather = async e => {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
+    console.log("log");
 
     const api_call = await fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`
@@ -26,41 +27,39 @@ class App extends React.Component {
 
     const response = await api_call.json();
     console.log(response);
-    if(city && country){
+    if (city && country) {
       this.setState({
-
         temperature: response.main.temp,
         city: response.name,
         country: response.sys.country,
         humidity: response.main.humidity,
         description: response.weather[0].description,
         error: ""
-
-      })
-    }else{
+      });
+    } else {
       this.setState({
-        error:"please input proper values"
-      })
+        error: "please input proper values"
+      });
     }
-  }
+  };
 
-  render(){
-    return(
-      <div> 
-        <Form loadWeather = {this.getWeather} />
+  render() {
+    return (
+      <div className="container">
+        <Titles />
+        <Form loadWeather={this.getWeather} />
+        
 
         <Weather>
-           temperature={this.state.temperature}
-           city={this.state.city}
-           country={this.state.country}
-           humidity={this.state.humidity}
-           description={this.state.description}
-           error={this.state.error}
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
         </Weather>
-
       </div>
-    )
+    );
   }
-
 }
 export default App;
