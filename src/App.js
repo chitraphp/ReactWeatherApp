026afd03e50b76cbac20305551ae334449326bd3
Chitra Atmakuru <chitraphp@users.noreply.github.com>
2020-02-13 09:40:38 -0800

    add log response

--- src/App.js
+++ src/App.js
@@ -19,13 +19,14 @@ class App extends React.Component {
   getWeather = async e => {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    console.log("log");
+    console.log(city);
 
     const api_call = await fetch(
       `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`
     );
 
     const response = await api_call.json();
+    console.log(response.name);
     console.log(response);
     if (city && country) {
       this.setState({
